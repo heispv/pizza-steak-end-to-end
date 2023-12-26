17ffe3ef179ed12181436ec59e9ef004673a722c
heispv <peymanvahidi1998@gmail.com>
2023-12-26 18:36:31 +0100

    folder structure

---
+++ template.py
@@ -0,0 +1,40 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format="[%(asctime)s] - %(message)s")
+
+project_name = "PizzaSteakClassifier"
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "dvc.yaml",
+    "params.yaml",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb"
+]
+
+for filepath in list_of_files:
+    path = Path(filepath)
+    file_dir, file_name = os.path.split(filepath)
+
+    if file_dir != "":
+        os.makedirs(file_dir, exist_ok=True)
+        logging.info(f"Created directory: {file_dir} for the file: {filepath}")
+    
+    if (not os.path.exists(path)) or (os.path.getsize(path) == 0):
+        with open(filepath, "w") as f:
+            f.write("")
+            logging.info(f"Created empty file: {filepath}")
+    else:
+        logging.info(f"File already exists: {filepath}")
+    
