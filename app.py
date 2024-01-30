df7e87421b866e4cc544ee168289e04a3ed30355
heispv <peymanvahidi1998@gmail.com>
2024-01-30 17:00:48 +0100

    add front and backend

--- app.py
+++ app.py
@@ -46,3 +46,4 @@ if __name__ == "__main__":
     # app.run(host='0.0.0.0', port=8080) #local host
     # app.run(host='0.0.0.0', port=8080) #for AWS
     app.run(host='0.0.0.0', port=80) #for AZURE
+    
